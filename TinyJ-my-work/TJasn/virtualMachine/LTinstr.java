57ce895a486faf31e1bc4163e66443e8ee64ebfb
Arton dz <161343584+waroffire2@users.noreply.github.com>
2026-09-14 17:20:29 -0400

    Add TinyJ virtual machine instruction implementations

---
+++ TinyJ-my-work/TJasn/virtualMachine/LTinstr.java
@@ -0,0 +1,17 @@
+package TJasn.virtualMachine;
+
+import static TJasn.virtualMachine.CodeInterpreter.*;
+
+public class LTinstr extends ZeroOperandInstruction {
+
+  void execute ()
+  {
+    /* ???????? */
+	  EXPRSTACK[--ESP-1] =(EXPRSTACK[ESP-1] < EXPRSTACK[ESP]) ? 1 : 0;
+  }
+
+  public LTinstr ()
+  {
+    super("LT");
+  }
+}
---
+++ TinyJ-my-work/TJasn/virtualMachine/ORinstr.java
@@ -0,0 +1,18 @@
+package TJasn.virtualMachine;
+
+import static TJasn.virtualMachine.CodeInterpreter.*;
+
+public class ORinstr extends ZeroOperandInstruction {
+
+  void execute ()
+  {
+    /* ???????? */
+	  EXPRSTACK[--ESP-1] |= EXPRSTACK[ESP];
+  }
+
+  public ORinstr ()
+  {
+    super("OR");
+  }
+}
+
